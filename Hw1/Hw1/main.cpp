bb30be453333eaaed2e6980c79f52a0ef333d67a
Humanphyti <humanphyti@gmail.com>
2016-09-13 13:49:25 -0700

    From Eben

--- Hw1/Hw1/main.cpp
+++ Hw1/Hw1/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World! Im in CS 172\n";
     std::cout << "Here is another line\n";
+    std::cout << "Hello";
     return 0;
 }
